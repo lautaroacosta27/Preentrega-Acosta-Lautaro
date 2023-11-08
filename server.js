5ade8373bff52ff4b7ca068099d23d86f1494273
lautaroacosta27 <acostalautaro08@gmail.com>
2023-11-08 15:16:27 -0300

    agramos detalles a la api

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
-const btn = document.getElementById('btnFinalizarReserva');
+const btnReservar = document.getElementById('btnReservar');
+const btnFinalizarReserva = document.getElementById('btnFinalizarReserva');
 const div = document.getElementById('characters');
 
-btn.addEventListener('click', () => {
-  console.log('fetch api');
+btnReservar.addEventListener('click', () => {
+    // Ocultar imágenes al presionar el botón "RESERVAR"
+    div.style.display = 'none';
+});
 
-  fetch('https://rickandmortyapi.com/api/character')
-  .then((response)=> response.json())
-  .then((data)=> renderCharacters(data.results));
+btnFinalizarReserva.addEventListener('click', () => {
+    // Mostrar imágenes al presionar el botón "Finalizar reserva"
+    fetch('https://rickandmortyapi.com/api/character')
+        .then((response) => response.json())
+        .then((data) => renderCharacters(data.results));
 });
 
 function renderCharacters(characters) {
-  characters.forEach(ch =>{
-    div.innerHTML += `<img src="${ch.image}">`;
-  });
+    // Limpiar el contenido anterior antes de mostrar nuevas imágenes
+    div.innerHTML = '';
+    
+    characters.forEach(ch => {
+        div.innerHTML += `<img src="${ch.image}">`;
+    });
+
+    // Mostrar las imágenes después de cargarlas
+    div.style.display = 'block';
 }
