1a8c088c66b1782342c7f837b39db0fe3ce502b0
lautaroacosta27 <acostalautaro08@gmail.com>
2023-11-08 15:07:32 -0300

    AGREGAMOS API

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 $(document).ready(function() {
-  const reservaForm = $("#reservaForm");
-  const reservaDetails = $("#reservaDetails");
+    const reservaForm = $("#reservaForm");
+    const reservaDetails = $("#reservaDetails");
 
-  $("#btnReservar").click(function() {
-      reservaForm.removeClass("hidden");
-      reservaDetails.addClass("hidden");
-  });
+    $("#btnReservar").click(function() {
+        reservaForm.removeClass("hidden");
+        reservaDetails.addClass("hidden");
+    });
 
-  reservaForm.submit(function(e) {
+    $("#btnFinalizarReserva").click(function(e) {
       e.preventDefault();
 
+      // Obtener los valores del formulario
       const nombreUsuario = $("#nombreUsuario").val();
       const apellidoUsuario = $("#apellidoUsuario").val();
       const fecha = $("#fecha").val();
@@ -24,90 +25,64 @@ $(document).ready(function() {
       const cantidadPersonasInt = parseInt(cantidadPersonas, 10);
 
       if (cantidadPersonasInt < 1 || cantidadPersonasInt > 8) {
-          $("#cantidadPersonasError").text("La cantidad de personas debe estar entre 1 y 8.");
-          formValid = false;
+        $("#cantidadPersonasError").text("La cantidad de personas debe estar entre 1 y 8.");
+        formValid = false;
       } else {
-          $("#cantidadPersonasError").text("");
+        $("#cantidadPersonasError").text("");
       }
 
       if (formValid) {
-          const menus = [
-              {
-                  entrada: "Croquetas",
-                  platoPrincipal: "Asado",
-                  bebida: "Gaseosa y Cerveza",
-                  postre: "Helado"
-              },
-              {
-                  entrada: "Rabas",
-                  platoPrincipal: "Pasta",
-                  bebida: "Gaseosa y Vino",
-                  postre: "Helado"
-              }
-          ];
-
-          if (opcionMenu === "1" || opcionMenu === "2") {
-              const menuElegido = menus[opcionMenu - 1];
-
-              $("#nombreApellido").text(nombreUsuario + " " + apellidoUsuario);
-              $("#fechaHora").text( fecha + "  " + hora + "hs");
-              $("#cantidadPersonasReserva").text(cantidadPersonas);
-
-              $("#nombresPersonas").empty();
-              $("#nombresPersonas").append("<li>" + nombrePersona + "</li>");
-
-              $("#menuElegido").empty();
-              $("#menuElegido").append("<li>Entrada: " + menuElegido.entrada + "</li>");
-              $("#menuElegido").append("<li>Plato principal: " + menuElegido.platoPrincipal + "</li>");
-              $("#menuElegido").append("<li>Bebida: " + menuElegido.bebida + "</li>");
-              $("#menuElegido").append("<li>Postre: " + menuElegido.postre + "</li");
-
-              reservaDetails.removeClass("hidden");
-              reservaForm.addClass("hidden");
-
-              // Guardar datos en el almacenamiento local (localStorage)
-              localStorage.setItem("nombreUsuario", nombreUsuario);
-              localStorage.setItem("apellidoUsuario", apellidoUsuario);
-              localStorage.setItem("fechaReserva", fecha);
-              localStorage.setItem("horaReserva", hora);
+        const menus = [
+          {
+            entrada: "Croquetas",
+            platoPrincipal: "Asado",
+            bebida: "Gaseosa y Cerveza",
+            postre: "Helado"
+          },
+          {
+            entrada: "Rabas",
+            platoPrincipal: "Pasta",
+            bebida: "Gaseosa y Vino",
+            postre: "Helado"
           }
-          const detallesReserva = {
-            nombre: nombreUsuario,
-            apellido: apellidoUsuario,
-            fecha: fecha,
-            hora: hora,
-            cantidadPersonas: cantidadPersonas,
-            nombrePersona: nombrePersona,
-            opcionMenu: opcionMenu
-        };
-        $.ajax({
-            type: 'POST',
-            url: 'http://localhost:3000/send-email', // Reemplaza con la URL de tu API
-            data: JSON.stringify(detallesReserva),
-            contentType: 'application/json',
-            success: function(response) {
-              console.log('Correo enviado con éxito:', response);
-              // Aquí podrías mostrar un mensaje al usuario indicando que la reserva se realizó y el correo se envió
-            },
-            error: function(error) {
-              console.error('Error al enviar el correo:', error);
-              // Manejar el error, mostrar un mensaje al usuario, etc.
-            }
-          });
+        ];
+
+        if (opcionMenu === "1" || opcionMenu === "2") {
+            const menuElegido = menus[opcionMenu - 1]
+
+          // Mostrar los detalles de la reserva
+          $("#nombreApellido").text(nombreUsuario + " " + apellidoUsuario);
+          $("#fechaHora").text(fecha + " " + hora + "hs");
+          $("#cantidadPersonasReserva").text(cantidadPersonas);
+          $("#nombresPersonas").empty();
+          $("#nombresPersonas").append("<li>" + nombrePersona + "</li>");
+          $("#menuElegido").empty();
+          $("#menuElegido").append("<li>Entrada: " + menuElegido.entrada + "</li>");
+          $("#menuElegido").append("<li>Plato principal: " + menuElegido.platoPrincipal + "</li>");
+          $("#menuElegido").append("<li>Bebida: " + menuElegido.bebida + "</li>");
+          $("#menuElegido").append("<li>Postre: " + menuElegido.postre + "</li>");
+
+          reservaForm.addClass("hidden");
+          reservaDetails.removeClass("hidden");
+          // Guardar datos en el almacenamiento local (localStorage)
+          localStorage.setItem("nombreUsuario", nombreUsuario);
+          localStorage.setItem("apellidoUsuario", apellidoUsuario);
+          localStorage.setItem("fechaReserva", fecha);
+          localStorage.setItem("horaReserva", hora);
+        }
       }
+    });
+
+      // Recuperar datos almacenados en localStorage y mostrarlos en outputDiv
+      let nombreRecuperado = localStorage.getItem("nombreUsuario");
+      let apellidoRecuperado = localStorage.getItem("apellidoUsuario");
+      let fechaRecuperada = localStorage.getItem("fechaReserva");
+      let horaRecuperada = localStorage.getItem("horaReserva");
+
+      let outputDiv = $("#outputDiv");
+      outputDiv.append("Datos almacenados:");
+      outputDiv.append("Nombre: " + nombreRecuperado);
+      outputDiv.append("Apellido: " + apellidoRecuperado);
+      outputDiv.append("Fecha de reserva: " + fechaRecuperada);
+      outputDiv.append("Hora de reserva: " + horaRecuperada);
   });
-
-
-  // Recuperar datos almacenados en localStorage y mostrarlos en outputDiv
-  let nombreRecuperado = localStorage.getItem("nombreUsuario");
-  let apellidoRecuperado = localStorage.getItem("apellidoUsuario");
-  let fechaRecuperada = localStorage.getItem("fechaReserva");
-  let horaRecuperada = localStorage.getItem("horaReserva");
-
-  let outputDiv = $("#outputDiv");
-  outputDiv.append("Datos almacenados:");
-  outputDiv.append("Nombre: " + nombreRecuperado);
-  outputDiv.append("Apellido: " + apellidoRecuperado);
-  outputDiv.append("Fecha de reserva: " + fechaRecuperada);
-  outputDiv.append("Hora de reserva: " + horaRecuperada);
-});
--- server.js
+++ server.js
@@ -1,53 +1,16 @@
-const express = require('express');
-const nodemailer = require('nodemailer');
-const app = express();
-const PORT = 3000;
+const btn = document.getElementById('btnFinalizarReserva');
+const div = document.getElementById('characters');
 
-const transporter = nodemailer.createTransport({
-  service: 'Gmail',
-  auth: {
-    user: 'acostalautaro08@gmail.com', // Coloca tu dirección de correo de Gmail
-    pass: '43906806' // La contraseña de tu cuenta de Gmail
-  }
+btn.addEventListener('click', () => {
+  console.log('fetch api');
+
+  fetch('https://rickandmortyapi.com/api/character')
+  .then((response)=> response.json())
+  .then((data)=> renderCharacters(data.results));
 });
 
-app.use(express.json());
-
-app.post('/send-email', (req, res) => {
-  const {
-    nombreUsuario,
-    apellidoUsuario,
-    fecha,
-    hora,
-    cantidadPersonas,
-    nombrePersona,
-    opcionMenu
-  } = req.body;
-
-  const mailOptions = {
-    from: 'acostalautaro08@gmail.com', // Remitente
-    to: 'acostalautaro08@gmail.com', // Destinatario
-    subject: 'Detalles de la reserva en Casa Ruda',
-    text: `Detalles de la reserva:\n
-           Nombre: ${nombreUsuario} ${apellidoUsuario}\n
-           Fecha: ${fecha}\n
-           Hora: ${hora}\n
-           Cantidad de personas: ${cantidadPersonas}\n
-           Nombre de la persona: ${nombrePersona}\n
-           Opción de menú elegida: ${opcionMenu}`
-  };
-
-  transporter.sendMail(mailOptions, function(error, info) {
-    if (error) {
-      console.log(error);
-      res.status(500).send('Error al enviar el correo');
-    } else {
-      console.log('Correo enviado: ' + info.response);
-      res.status(200).send('Correo enviado correctamente');
-    }
+function renderCharacters(characters) {
+  characters.forEach(ch =>{
+    div.innerHTML += `<img src="${ch.image}">`;
   });
-});
-
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
+}
